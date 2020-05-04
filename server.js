4d4772f2d9780d56f4cf833c342e5b9f38e66d15
Nikita Malyschkin <nikita.malyschkin@googlemail.com>
2020-05-04 13:55:51 +0200

    v0

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use("/public", express.static(path.resolve(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+app.listen(3000, () => {
+    console.log("server running on http://localhost:3000");
+});
